3c2ab6fd3034fb02e8ca7b8a4a0e36afcb634d9a
lakshyagupta8383 <guptalakshya8383@gmail.com>
2026-02-15 04:51:04 +0530

    chore: remove backup directory and add config structure

--- backend_backup/src/modules/batch/batch.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyFirebase = require("../../middlewares/verifyFirebase");
-const { createBatch } = require("./batch.controller");
-
-router.post("/", verifyFirebase, createBatch);
-
-module.exports = router;
